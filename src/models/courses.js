b0180a18912bc063abd15d660532dc814a9b0ceb
Suliman Khan <sulimank418@gmail.com>
2022-11-26 13:12:27 +0500

    Done

--- src/models/courses.js
+++ src/models/courses.js
@@ -3,9 +3,9 @@ const courseSchema = new mongoose.Schema({
     name: String,
     description: String,
     status: String,
-    package: [{type:mongoose.Schema.Types.ObjectId,ref:'Package'}],
-    price:{
-        type:Number
+    package: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Package' }],
+    price: {
+        type: Number
     }
 }, {
     timestamps: true
