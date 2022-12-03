878a165c63fcac514cdaca4355af84bc8b08f397
Suliman Khan <sulimank418@gmail.com>
2022-12-03 13:24:44 +0500

    Quiz final

---
+++ src/models/result.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const Result = mongoose.model('result', new mongoose.Schema({
+    quiz: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'quiz'
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    correct_ans: Array,
+    wrong_ans: Array,
+    points: Number,
+    totalQuestions: Number,
+    time: String,
+    ans: {
+        type: String,
+        // convert database data to Object from string
+        get: function (data) {
+            try {
+                return JSON.parse(data)
+            } catch (error) {
+                console.log("Mongoose Result:", error.message)
+            }
+        },
+        // convert to string
+        set: function (data) {
+            return JSON.stringify(data)
+        }
+    }
+}))
+module.exports = Result
